1dcba35af58291f043d20225367b4235e9c63080
Alec_Gracelli <alecgracelli@gmail.com>
2022-11-18 13:46:20 -0300

    Create new Lesson

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.Write($"Funcionário: {funcionario}");
+
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine();
+            Console.Write($"Dados atualizados: {funcionario}");
+        }
+    }
+}
